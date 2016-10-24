a28a17206af6f6e5c2ad6bbf8e18e2a8aeb6d45d
Arnaldo Pedroso <apedros@L-BRZN6HRG3Q-M.s08771.gec.br.wal-mart.com>
2016-10-24 12:31:19 -0200

    .

--- scripts/roda.sql
+++
@@ -1,4 +0,0 @@
-select count(*) from PUBOWN.PB_HISTORICO;
-select * from PUBOWN.PB_BLOCO_UTILIZACAO;
-select count(*) from PUBOWN.PB_USUARIO_PERFIL;
-alter system flush shared_pool;
--- scripts/roda1.sql
+++
@@ -1,3 +0,0 @@
-select * from PUBOWN.PB_MATERIA;
-select * from PUBOWN.PB_MATERIA_PALAVRA_CHAVE;
-alter system flush shared_pool;
